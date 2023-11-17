a8354a51a8d7e0dc30ef6f4974aff5b850031763
Andrew <andrewp3773@gmail.com>
2023-11-16 20:56:05 -0800

    CRUD for users

--- app.js
+++ app.js
@@ -34,6 +34,110 @@ app.get('/', function(req, res)
         })
     });   
 
+/*---------------------------------------------------------------------------------------
+Implementation of Users page
+---------------------------------------------------------------------------------------*/
+
+app.get('/users', function(req,res) {
+    let usersDisplay = `SELECT * FROM Users;`;
+
+    db.pool.query(usersDisplay, function(error, rows, feilds) {
+        res.render('users', {usersData: rows})
+    })
+});
+
+app.post('/add-user-ajax', function(req,res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let phone = parseInt(data.phone);
+    if (isNaN(phone))
+    {
+        phone = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Users (email, name, address, phone) VALUES ( '${data.email}', '${data.name}', '${data.address}', '${data.phone}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Users;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+app.delete('/delete-user-ajax/', function(req,res,next){
+    let data = req.body;
+    let userID = parseInt(data.userID);
+    let deleteUser = `DELETE FROM Users WHERE userID = ?`;
+  
+          // Run the 1st query
+          db.pool.query(deleteUser, [userID], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              } else {
+                res.sendStatus(204);
+            }
+              
+  })});
+
+  app.put('/put-user-ajax', function(req,res,next){
+    let data = req.body;
+  
+    let homeworld = parseInt(data.homeworld);
+    let person = parseInt(data.fullname);
+
+    let name = parseInt(data.name);
+    let email = parseInt(data.email);
+    let address = parseInt(data.address);
+    let phone = parseInt(data.phone);
+  
+    let updateUserQuery = `UPDATE Users SET email = ?, address = ?, phone = ? WHERE Users.name = ?`
+  
+          // Run the 1st query
+          db.pool.query(updateUserQuery, [email, address, phone], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+
+            } else {
+                res.send(rows);
+            }
+  })});
+
+
+
+
 app.post('/add-appliance-ajax', function(req, res) 
 {
     // Capture the incoming data and parse it back to a JS object
---
+++ public/js/add_user.js
@@ -0,0 +1,125 @@
+// Get the objects we need to modify
+let addUserForm = document.getElementById('addUser');
+
+// Modify the objects we need
+addUserForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let nameInput = document.getElementById("nameInput");
+    let emailInput = document.getElementById("emailInput");
+    let addressInput = document.getElementById("addressInput");
+    let phoneInput = document.getElementById("phoneInput");
+
+    // Get the values from the form fields
+    let nameValue = nameInput.value;
+    let emailValue = emailInput.value;
+    let addressValue = addressInput.value;
+    let phoneValue = phoneInput.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        email: emailValue, 
+        name: nameValue,
+        address: addressValue,
+        phone: phoneValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-user-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            nameInput.value = '';
+            emailInput.value = '';
+            addressInput.value = '';
+            phoneInput.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("userTable");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let editCell = document.createElement("TD");
+    let delCell = document.createElement("TD");
+    let idCell = document.createElement("TD");
+    let nameCell = document.createElement("TD");
+    let emailCell = document.createElement("TD");
+    let addressCell = document.createElement("TD");
+    let phoneCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    //editCell.innerHTML = '<a href="#" onClick="updateUser({{this.userID}})">Edit</a>';
+    //delCell.innerHTML = '<a href="#" onclick="deleteUser()">Delete</a>'
+    idCell.innerText = newRow.userID;
+    nameCell.innerText = newRow.name;
+    emailCell.innerText = newRow.email;
+    addressCell.innerText = newRow.address;
+    phoneCell.innerText = newRow.phone;
+
+    delCell = document.createElement("button");
+    delCell.innerHTML = "Delete";
+    delCell.onclick = function(){deleteUser(newRow.userID);}
+/*
+    delA = document.createElement("a");
+    delA.innerHTML = "Delete";
+    delA.href = "#";
+    delA.onclick = function() {
+        deleteUser(newRow.userID)
+    };
+    delCell.appendChild(delA);
+*/
+
+    // Add the cells to the row 
+    row.appendChild(editCell);
+    row.appendChild(delCell);
+    row.appendChild(idCell);
+    row.appendChild(nameCell);
+    row.appendChild(emailCell);
+    row.appendChild(addressCell);
+    row.appendChild(phoneCell);
+    
+    row.setAttribute('data-value', newRow.id)
+    // Add the row to the table
+    currentTable.appendChild(row);
+
+    // Find drop down menu, create a new option, fill data in the option (full name, id),
+    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+    let selectMenu = document.getElementById("mySelect");
+    let option = document.createElement("option");
+    option.text = newRow.name;
+    option.value = newRow.id;
+    selectMenu.add(option);
+}
